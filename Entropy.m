a83b3521f5b6779d8501e48497cf01862c5eef2f
Maurax <marcus3111@gmail.com>
2016-04-06 20:43:08 +0200

    Entropy function done

--- Entropy.m
+++ Entropy.m
@@ -7,17 +7,22 @@ function H=Entropy(P)
 % P row vector: Each position gives binary entropy function
 
 [m, n]=size(P);
-if([m, n]==[1 1])   %Scalar
-    if(P==0)
-        H=0;
-    else
-        H=-P*log2(P)-(1-P)*log2(1-P);
-    end
-elseif(n==1)        %Column vector
-    H=sum(-P.*log2(P));
-elseif(m==1)        %Row vector
-    H=-P.*log2(P)-(1-P).*log2(1-P);
+if [m n] == [1 1]   %Scalar
+    H=-P*lg2(P)-(1-P)*lg2(1-P);
+elseif n==1         %Column vector
+    H=sum(-P.*lg2(P));
+elseif m==1         %Row vector
+    H=-P.*lg2(P)-(1-P).*lg2(1-P);
 else                %matrix
-    H=sum(-P.*log2(P));
+    H=sum(-P.*lg2(P));
 end
+
+
+function a=lg2(n)
+a=arrayfun(@lg2_, n);
+
+function a = lg2_(n)
+if(n==0)
+    n=0.0001;
 end
+a=log2(n);
