f47b24f12b8066a2e99007485d5b747f4d64a917
Marcus Kindberg <tpi12mki@hacke-5.student.lth.se>
2016-04-06 14:28:09 +0200

    cant handle 0

---
+++ Entropy.m
@@ -0,0 +1,27 @@
+function H=Entropy(P)
+% The Entropy function H(X)
+%
+% P column vector: the vector is the probability distribution.
+% P matrix: Each column vector is interpreted as a probability distribution
+% P scalar: The binary entropy function of [P; 1-P]
+% P row vector: Each position gives binary entropy function
+
+[m, n]=size(P);
+if([m, n]==[1 1])   %Scalar
+    'scal'
+    if(P==0)
+        H=0;
+    else
+        H=-P*log2(P)-(1-P)*log2(1-P);
+    end
+elseif(n==1)        %Column vector
+    'col'
+    H=sum(-P.*log2(P));
+elseif(m==1)        %Row vector
+    'row'
+    H=-P.*log2(P)-(1-P).*log2(1-P);
+else                %matrix
+    'matrix'
+    H=sum(-P.*log2(P));
+end
+end
