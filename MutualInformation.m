06a8778504fc305d8f96bd5d36a98058a392bd75
Marcus Kindberg <tpi12mki@philon-05.control.lth.se>
2016-04-07 12:58:05 +0200

    fixed last error

--- MutualInformation.m
+++ MutualInformation.m
@@ -4,8 +4,17 @@ function I=MutualInformation(P)
 % P=P(X,Y) is the joint probability of X and Y.
 [m, n]=size(P);
 I=0;
-for i=0:m
-    for j=0:n
-        I=I+P(m, n)*log2(P(m, n)/(sum(P(m, :))*sum(P(:, n))));
+for i=1:m
+    for j=1:n
+        I=I + P(i, j) * lg2(P(i, j)/(sum(P(i, :))*sum(P(:, j))));
     end
 end
+
+function a=lg2(n)
+a=arrayfun(@lg2_, n);
+
+function a = lg2_(n)
+if(n==0)
+    n=0.0001;
+end
+a=log2(n);
